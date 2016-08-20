5db490e2af7eea70a7475006a27493f510fbb826
rbn42 <bl100@students.waikato.ac.nz>
2016-08-20 16:23:01 +1200

    flip

---
+++ flip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Greet
+
+Usage:
+  main.py <path_root> 
+  main.py -h | --help
+
+Options:
+  -h --help     Show this screen.
+"""
+from docopt import docopt
+arguments = docopt(__doc__)
+
+import glob
+import os.path
+from PIL import Image
+
+path_root = arguments['<path_root>']
+imgs = glob.glob('%s/*.png' % path_root)
+for path in imgs:
+    dst = '%s_flip.png' % path
+    if not os.path.exists(dst):
+        print('convert -flop  "%s" "%s"' % (path, dst))
--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ mkdir ./resize_rotate_left
 python ./bin/resize.py ./raw ./resize 1920x1080 #分辨率
 #bash ./bin/resize.sh 3840x2160 #分辨率
 
+#flip
+python ./bin/flip.py ./resize | bash
+
 python ./bin/rotate.py ./resize ./resize_rotate  right
 python ./bin/rotate.py ./resize ./resize_rotate_left left
 #python ./bin/rotate.py ./raw_ ./resize_rotate right
