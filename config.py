676a9d66a7fca616eb7218978ba6db8939d825ea
rbn42 <bl100@students.waikato.ac.nz>
2017-04-18 19:29:05 +1200

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 COMPIZ_ALONE = False
 COMPIZ0_8 = True
-CONFIG_FILE = '~/config/wallpapers/config.json'
+#CONFIG_FILE = '~/config/wallpapers/config.json'
+# 加载config那边自带了json后缀
+CONFIG_FILE = '~/config/wallpapers/config'  # .json'
