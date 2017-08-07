6a8d5c342bfb2090edc35771ae7a226490c12d6d
rbn42 <bl100@students.waikato.ac.nz>
2017-08-08 10:36:23 +1200

    get screen orientation

--- compiz_wallpaper.py
+++ compiz_wallpaper.py
@@ -1,18 +1,30 @@
 import glob
-import random
 import os
+import random
 import sys
+import subprocess
+import re
 
 
 def getconfig(num=1, root='.'):
+
+    cmd = "xrandr --query --verbose"
+    s = subprocess.check_output(cmd, shell=True).decode()
+    for line in s.split('\n'):
+        if 'disconnected' not in line:
+            if 'connected' in line:
+                r = '\) (.+) \('
+                orientation = re.findall(r, line)[0]
+                break
+
     # find images
     exts = 'jpg', 'jpeg', 'png', 'gif'
-    if os.path.exists(root + '/mark_rotate_normal'):
+    if 'normal' == orientation:
         l = [glob.glob(root + '/raw_/*.' + n) for n in exts]
         l += [glob.glob(root + '/resize/*.' + n) for n in exts]
-    elif os.path.exists(root + '/mark_rotate_right'):
+    elif 'right' == orientation:
         l = [glob.glob(root + '/resize_rotate/*.' + n) for n in exts]
-    elif os.path.exists(root + '/mark_rotate_left'):
+    elif 'left' == orientation:
         l = [glob.glob(root + '/resize_rotate_left/*.' + n) for n in exts]
     imgs = []
     for ll in l:
