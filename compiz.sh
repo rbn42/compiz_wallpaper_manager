75ebb4d548da72f2b526f6633a2d5f5747fc9bbb
rbn42 <bl100@students.waikato.ac.nz>
2017-10-19 02:49:48 +1300

    randomize

--- compiz.sh
+++ compiz.sh
@@ -3,10 +3,10 @@
 #* * * * *   bash ~/Pictures/compiz/bin/compiz.sh
 
 #壁纸目录
-WALLPAPER_ROOT="$1"
+#WALLPAPER_ROOT="$1"
 
 #加载壁纸
-python $(dirname "$0")/compiz.py $WALLPAPER_ROOT | bash
+python $(dirname "$0")/compiz.py "$@" | bash
 
 #compiz0.8用
 python ~/git/compiz_config/main.py ~/.config/compiz/compizconfig/Default.ini submit
